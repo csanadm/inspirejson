22c8a3905ce5f607a892d58d3e58fe36df308707
csanadm <38218165+csanadm@users.noreply.github.com>
2020-06-11 12:07:46 +0200

    Create printInspirePublist.php

---
+++ printInspirePublist.php
@@ -0,0 +1,84 @@
+<?php
+function printInspirePublist($url,$Nmaxauth)
+{
+  if($Nmaxauth<1) $Nmaxauth = 1; // MAX OF AUTHORS TO BE PRINTED
+
+  // OPEN FILE AND DECODE JSON CONTENTS
+  $file = file_get_contents($url);
+  if($file === FALSE) return ;
+  $json = json_decode($file);
+
+  // DETERMINE NUMBER OF RECORDS ASSOCIATED WITH QUERY
+  $Ntotalrec = $json->{"hits"}->{"total"};
+  $Nremainingrec = $Ntotalrec;
+
+  // START TO PROCESS RECORDS AND PRINT AS HTML LIST
+  echo "<ol>\n";
+  while($Nremainingrec>0)
+  {
+    // PROCESS RECORDS OF CURRENT JSON DATASET
+    $Nthisrec = count($json->{"hits"}->{"hits"});
+    for($irec=0;$irec<$Nthisrec;$irec++)
+    {
+      $Nremainingrec--;
+      echo "<li>\n";
+      $record = $json->{"hits"}->{"hits"}[$irec];                                   // RECORD CONTENTS
+      $id = $record->{"id"};                                                        // INSPIRE ID
+      $cited = $record->{"metadata"}->{"citation_count"};                           // CITATION COUNT
+      $indcited = $record->{"metadata"}->{"citation_count_without_self_citations"}; // INDEPENDENT CITATIONS
+      $authors = $record->{"metadata"}->{"authors"};                                // AUTHOR OBJECT
+
+      // CREATE PRINTABLE AUTHOR LIST
+      $Nauth = count($authors);
+      $authlist = "";
+      if($Nauth > $Nmaxauth)
+      {
+        $authlist = $authors[0]->{"full_name"}." <i>et al.</i>";
+      }
+      else
+      {
+        for($iauth = 0; $iauth < $Nauth; $iauth++)
+        {
+          $thisauthor = $authors[$iauth]->{"full_name"};
+          $authlist .= ($iauth==0?"":", ").$thisauthor;
+        }
+      }
+
+      $title = $record->{"metadata"}->{"titles"}[0]->{"title"};                     // PAPER TITLE
+      $texkey = $record->{"metadata"}->{"texkeys"}[0];                              // PAPER TEXKEY
+      $eprint = $record->{"metadata"}->{"arxiv_eprints"}[0]->{"value"};             // EPRINT ID
+      $arXclass = $record->{"metadata"}->{"arxiv_eprints"}[0]->{"categories"}[0];   // ARXIV CLASS
+      $doctype = $record->{"metadata"}->{"document_type"}[0];                       // DOCUMENT TYPE
+      $reportnumber = $record->{"metadata"}->{"report_numbers"}[0]->{"value"};      // REPORT NUMBER
+      $publinfo = $record->{"metadata"}->{"publication_info"};                      // PUBLICATION INFO OBJECT
+      $publother = $publinfo[0]->{"pubinfo_freetext"};                              // PUBLICATION INFO FREE TEXT
+      $journal = $publinfo[0]->{"journal_title"};                                   // JOURNAL
+      $volume = $publinfo[0]->{"journal_volume"};                                   // VOLUME
+      $year = $publinfo[0]->{"year"};                                               // YEAR
+      $page = $publinfo[0]->{"page_start"};                                         // PAGE
+      $artid = $publinfo[0]->{"artid"};                                             // PAPER ID
+      if($page == "") $page = $artid;
+
+      // PRINT RECORD
+      echo "<b><a href=\"http://inspirehep.net/literature/$id\">$title</a></b>,<br>\n";
+      echo "$authlist,<br>\n";
+      if($journal != "") echo "$journal <b>$volume</b> ($year) $page";
+      elseif($doctype == "thesis") echo "PhD thesis";
+      elseif($reportnumber != "") echo "$reportnumber";
+      else echo "$doctype".($publother!=""?", $publother":" [INSPIRE record #$id]");
+      if($eprint!="") echo ", [<a href=\"http://arxiv.org/abs/$eprint\">arXiv:$eprint</a>]";
+      if($cited>0) echo ", <a href=\"https://inspirehep.net/literature?q=refersto:recid:$id\">cited $cited</a>";
+      echo "<br>\n";
+      echo "</li>\n";
+    }
+
+    // CHECK IF ALL RECORDS DONE
+    if($Nremainingrec==0) break;
+    else
+    {
+      $file = file_get_contents($url);
+      $json = json_decode($file);
+    }
+  }
+  echo "</ol>\n";
+}
