d450e807ea0e4fdbae11b12fd77707630a140f25
csanadm <38218165+csanadm@users.noreply.github.com>
2020-06-11 12:17:55 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,5 @@
 <?php
+  include("printInspirePublist.php");
   $url = "https://inspirehep.net/api/literature?q=".rawurlencode("find a Steven.Weinberg.1")."&size=50";
   $Nmaxauth = 3;
   printInspirePublist($url,$Nmaxauth)
